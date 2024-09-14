beaea4072d2344127f2d12a91119cc2f2b83ce9e
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-09-14 15:55:22 +0100

    tests pass at least...ok...lets take this a bit further shall we

--- formulaic/materializers/arrow.py
+++ formulaic/materializers/arrow.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import narwhals as nw
 
 from collections.abc import Mapping
 from typing import TYPE_CHECKING, Any, Dict, Iterator, Sequence
@@ -18,7 +19,7 @@ class NarwhalsMaterializer(PandasMaterializer):
 
     @override
     def _init(self) -> None:
-        self.__data_context = LazyArrowTableProxy(self.data)
+        self.__data_context = LazyArrowTableProxy(nw.from_native(self.data))
 
     @override  # type: ignore
     @property
@@ -29,26 +30,22 @@ class NarwhalsMaterializer(PandasMaterializer):
 class LazyArrowTableProxy(Mapping):
     def __init__(self, table: pyarrow.Table):
         self.table = table
-        self.column_names = set(self.table.column_names)
+        self.column_names = set(self.table.columns)
         self._cache: Dict[str, pandas.Series] = {}
         self.index = pandas.RangeIndex(len(table))
 
     def __contains__(self, value: Any) -> Any:
-        breakpoint()
         return value in self.column_names
 
     def __getitem__(self, key: str) -> Any:
-        breakpoint()
         if key not in self.column_names:
             raise KeyError(key)
         if key not in self._cache:
-            self._cache[key] = self.table.column(key).to_pandas()
+            self._cache[key] = self.table[key].to_pandas()
         return self._cache[key]
 
     def __iter__(self) -> Iterator[str]:
-        breakpoint()
         return iter(self.column_names)
 
     def __len__(self) -> int:
-        breakpoint()
         return len(self.column_names)
